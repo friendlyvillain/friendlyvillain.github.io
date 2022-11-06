34ff389975b3e92fb92bb710702d128f260c0956
friendlyvillain <zero02505@gmail.com>
2022-11-06 17:13:56 +0900

    1st post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "github.io 블로그 첫 포스팅입니다",
+        "excerpt":"If you want to review the page on the local server, execute bundle exec jekyll serve on the command line.  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/simple_guide/",
+        "teaser": null
+      }]
