3e0a81603df26c6f85676819b3fd760f65af1232
friendlyvillain <zero02505@gmail.com>
2025-02-23 19:16:30 +0900

    event listner modify

--- _javascript/pwa/sw.js
+++ _javascript/pwa/sw.js
@@ -1,5 +1,23 @@
 importScripts('./assets/js/data/swconf.js');
 
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && "navigate" in client) {
+          client.navigate(client.url);
+        }
+      });
+    });
+});
+
+/*
 const purge = swconf.purge;
 const interceptor = swconf.interceptor;
 
@@ -90,3 +108,4 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+*/
